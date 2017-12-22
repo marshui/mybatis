ca6fa513bf44afdb9462ca18d0e330ea95b5da3b
marshui <1500530467@qq.com>
2017-12-22 12:38:11 +0800

    加载mybatis的核心配置文件，并创建一个sqlsession

---
+++ SessionUtils.java
@@ -0,0 +1,25 @@
+package cn.marsma.mybatis;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+public class SessionUtils {
+    private static SqlSessionFactory sqlSessionFactory;
+
+    static {
+        try {
+            InputStream inputStream = Resources.getResourceAsStream("mybatisConfig.xml");
+            sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static SqlSessionFactory getSqlSessionFactory() {
+        return sqlSessionFactory;
+    }
+}
